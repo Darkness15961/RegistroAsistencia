f2f0933a2083b56f011324a2b0240fc01cd60377
darkness15961 <60859960@continental.edu.pe>
2025-10-01 13:06:29 -0500

    listo para iniciar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import { createApp } from 'vue'
+import App from './app.vue'
+import '../css/app.css' // importa Tailwind
+
+createApp(App).mount('#app')
---
+++ resources/js/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-3xl font-bold text-blue-600">
+      Sistema de Registro de Asistencia
+    </h1>
+    <p class="mt-2 text-gray-600">
+      Proyecto: <span class="font-semibold">app-RegistroAsistencia</span>
+    </p>
+  </div>
+</template>
+
+<script setup>
+// Aquí van imports futuros como Navbar, Footer, etc.
+</script>
