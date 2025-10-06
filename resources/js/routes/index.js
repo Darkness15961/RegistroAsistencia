e3886b44fff5a0d0d06d3801005207e5054fcaf7
adosoyola <75389246@continental.edu.pe>
2025-10-06 11:38:29 -0500

    areas

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../pages/Login.vue';
 import Home from '../pages/Home.vue';
 import Horarios from '../pages/Horarios.vue';
+import Areas from '../pages/Areas.vue';
 import Asistencias from '../pages/Asistencias.vue';
 import Usuarios from '../pages/Usuarios.vue';
 import Empleados from '../pages/Empleados.vue';
@@ -13,6 +14,7 @@ const routes = [
   { path: '/login', name: 'login', component: Login },
   { path: '/home', name: 'home', component: Home },
   { path: '/horarios', name: 'horarios', component: Horarios },
+  { path: '/areas', name: 'areas', component: Areas },
   { path: '/asistencias', name: 'asistencias', component: Asistencias },
   { path: '/usuarios', name: 'usuarios', component: Usuarios },
   { path: '/empleados', name: 'empleados', component: Empleados },
