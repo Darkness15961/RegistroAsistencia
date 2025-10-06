843f9bf999d91cc65c799e306a8dce63067687fe
adosoyola <75389246@continental.edu.pe>
2025-10-06 11:28:00 -0500

    fontawesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './app.vue'
 import router from './routes/index.js' // importa el router
 import '../css/app.css' // importa Tailwind
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 createApp(App).use(router).mount('#app');
